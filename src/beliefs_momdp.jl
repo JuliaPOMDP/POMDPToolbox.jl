a2c448280b01767b857225af0b6d40fc1415c595
Maxim Egorov <maxgegorov@gmail.com>
2015-12-01 09:46:51 -0800

    updated to work with momdps

--- src/beliefs_momdp.jl
+++ src/beliefs_momdp.jl
@@ -3,71 +3,52 @@ if isdir(Pkg.dir("MOMDPs"))
 
 using MOMDPs
 
-function update_belief(pomdp::MOMDP, x::Int64, a::Int64, o::Int64)
-    yspace = part_obs_space(pomdp)
-    ystates = domain(yspace)
-    ns = length(collect(ystates))
-    b = DiscreteBelief(ns)
-    update_belief!(b, pomdp, a, o)
-    return b
+
+type DiscreteMOMDPUpdater <: BeliefUpdater
+    momdp::POMDP
 end
 
+create_belief(updater::DiscreteMOMDPUpdater) = DiscreteBelief(length(collect(domain(part_obs_space(updater.momdp)))))
+
+
 # Updates the belief for a MOMDP with x fully observable variables index
-function update_belief!(b::DiscreteBelief, pomdp::MOMDP, x::Int64, a::Int64, o::Int64)
+function update(updater::DiscreteMOMDPUpdater, bold::DiscreteBelief, a::Action, o::Observation, x, bnew::DiscreteBelief=create_belief(updater))
+    pomdp = updater.momdp
     # asset that number of part observable states is size of belief
     yspace = part_obs_space(pomdp)
     ystates = domain(yspace)
     @assert length(collect(ystates)) == b.n
     b.valid = true
-
+    # initialize distributions
     od = create_observation_distribution(pomdp)
-    td1 = create_partially_obs_transition(pomdp)
-    td2 = create_partially_obs_transition(pomdp)
-
-    belief = b.b
-    new_belief = b.bp
-    fill!(new_belief, 0.0)
-
+    td = create_partially_obs_transition(pomdp)
+    # initialize belief
+    fill!(bnew, 0.0)
+    # iterate through all the partially observable states
     for (i, yp) in enumerate(ystates)
-        b_sum = 0.0
-        transition!(td1, pomdp, x, yp, a) 
-        observation!(od, pomdp, x, yp, a)
-        for iy = 1:length(td1) 
-            p = weight(td1, iy)
-            if p > 0.0
-                y = index(td1, iy)
-                transition!(td2, pomdp, x, y, a) 
-                for jy = 1:length(td2) 
-                    pp = weight(td2, jy)        
-                    if pp > 0.0
-                        ypidx = index(td2, jy)
-                        if ypidx == yp
-                            b_sum += pp*belief[y] 
-                        end
-                    end
-                end
-            end
+        # update the distributions
+        od = observation(pomdp, x, yp, a, od)
+        # get prob of observation o from current distribution
+        probo = pdf(od, o)
+        # if observation prob is 0.0, then skip rest of update b/c bnew[i] is zero
+        probo == 0.0 ? (continue) : (nothing)
+        b_sum = 0.0 # belief for state sp
+        for (j, y) in enumerate(ystates) 
+            td = transition(pomdp, x, y, a, td)
+            pp = pdf(td, yp)
+            b_sum += pp * bold[j]
         end
-        for io = 1:length(od)
-            idx = index(od, io)
-            if idx == o
-                new_belief[i] = weight(od, io)*b_sum
-                break
-            end
-        end
+        bnew[i] = probo * b_sum
     end
     norm = sum(new_belief)
-    # if norm is zero, the update was invalid - reset to old
-    if norm <= 0.0
-        b.valid = false
-        u = 1.0/length(b)
-        fill!(b, u)
+    # if norm is zero, the update was invalid - reset to uniform
+    if norm == 0.0
+        u = 1.0/length(bnew)
+        fill!(bnew, u)
     else
-        for i = 1:length(new_belief) new_belief[i] /= norm end
-        copy!(belief, new_belief)
-        #belief[1:end] = new_belief[1:end]
+        for i = 1:length(bnew); bnew[i] /= norm; end
     end
-    b
+    bnew
 end
 
 end # if check for MOMDPs
