a84305c1a53ce8fbea450d1f04d707b4f1a20468
Maxim Egorov <maxgegorov@gmail.com>
2017-01-24 23:13:40 -0800

    added unit tests

--- test/test_belief.jl
+++ test/test_belief.jl
@@ -1,5 +1,6 @@
 using POMDPToolbox
 using POMDPModels
+using Base.Test
 
 pomdp = TigerPOMDP()
 bu = DiscreteUpdater(pomdp)
@@ -8,3 +9,7 @@ bold = initialize_belief(bu, initial_state_distribution(pomdp), create_belief(bu
 a = 0
 o = true
 bnew = update(bu, bold, a, o)
+
+@test_approx_eq bnew.b [0.15, 0.85]
+@test_approx_eq pdf(bnew, 1) 0.15
+@test_approx_eq pdf(bnew, 2) 0.85
