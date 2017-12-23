c8357417a7a0483c2d8b623ac74fdf4a8bbd73a3
Zachary Sunberg <sunbergzach@gmail.com>
2017-12-23 15:28:43 -0800

    added warning for run_parallel with nprocs = 1

--- test/test_parallel.jl
+++ test/test_parallel.jl
@@ -11,12 +11,20 @@ let
     push!(q, Sim(pomdp, fwc, max_steps=32, rng=MersenneTwister(4), metadata=Dict(:policy=>"feed when crying")))
     push!(q, Sim(pomdp, rnd, max_steps=32, rng=MersenneTwister(4), metadata=Dict(:policy=>"random")))
 
+    println("There should be a warning here:")
+    run_parallel(q)
+
+    procs = addprocs(1)
+    @everywhere using POMDPToolbox
+    @everywhere using POMDPModels
+    println("There should not be a warning here:")
     @show run_parallel(q) do sim, hist
         return [:steps=>n_steps(hist), :reward=>discounted_reward(hist)]
     end
 
     @show data = run_parallel(q)
     @test data[1, :reward] == data[2, :reward]
+    rmprocs(procs)
 
     mdp = GridWorld()
     q = []
