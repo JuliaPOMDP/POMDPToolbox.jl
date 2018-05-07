bf66b5f2b943e1cb37403cf174c10fcfb1c31182
MaximeBouton <maxim.bouton@gmail.com>
2018-05-07 22:51:29 +0200

    style improvements

--- src/model/underlying_mdp.jl
+++ src/model/underlying_mdp.jl
@@ -10,23 +10,16 @@ function UnderlyingMDP{S, A, O}(pomdp::POMDP{S, A, O})
 end
 
 POMDPs.transition{P, S, A}(mdp::UnderlyingMDP{P, S, A}, s::S, a::A) = transition(mdp.pomdp, s, a)
-
 POMDPs.initial_state_distribution(mdp::UnderlyingMDP) = initial_state_distribution(mdp.pomdp)
-
-POMDPs.generate_s{P, S, A}(mdp::UnderlyingMDP{P, S, A}, s::S, a::A, rng::AbstractRNG) = generate_s(mdp.pomdp, s, a, rng)
-
-POMDPs.generate_sr{P, S, A}(mdp::UnderlyingMDP{P, S, A}, s::S, a::A, rng::AbstractRNG) = generate_sr(mdp.pomdp, s, a, rng)
-
+POMDPs.generate_s(mdp::UnderlyingMDP, s, a, rng::AbstractRNG) = generate_s(mdp.pomdp, s, a, rng)
+POMDPs.generate_sr(mdp::UnderlyingMDP, s::S, a::A, rng::AbstractRNG) = generate_sr(mdp.pomdp, s, a, rng)
 POMDPs.initial_state(mdp::UnderlyingMDP, rng::AbstractRNG) = initial_state(mdp.pomdp, rng)
-
 POMDPs.states(mdp::UnderlyingMDP) = states(mdp.pomdp)
 POMDPs.actions(mdp::UnderlyingMDP) = actions(mdp.pomdp)
-
 POMDPs.reward{P, S, A}(mdp::UnderlyingMDP{P, S, A}, s::S, a::A) = reward(mdp.pomdp, s, a)
 POMDPs.reward{P, S, A}(mdp::UnderlyingMDP{P, S, A}, s::S, a::A, sp::S) = reward(mdp.pomdp, s, a, sp)
 POMDPs.isterminal{P, S, A}(mdp ::UnderlyingMDP{P, S, A}, s::S) = isterminal(mdp.pomdp, s)
 POMDPs.discount(mdp::UnderlyingMDP) = discount(mdp.pomdp)
-
 POMDPs.n_actions(mdp::UnderlyingMDP) = n_actions(mdp.pomdp)
 POMDPs.n_states(mdp::UnderlyingMDP) = n_states(mdp.pomdp)
 POMDPs.state_index{P, S, A}(mdp::UnderlyingMDP{P, S, A}, s::S) = state_index(mdp.pomdp, s)
