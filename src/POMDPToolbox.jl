6fd7726afbc2b58395d5d03054f192d9a012bce1
Zachary Sunberg <sunbergzach@gmail.com>
2018-09-20 13:18:11 -0700

    added the file back

---
+++ src/POMDPToolbox.jl
@@ -0,0 +1,7 @@
+module POMDPToolbox
+
+function __init__()
+    @warn("Importing POMDPToolbox. As of Julia 0.7 POMDPToolbox has been discontinued. Please use POMDPModelTools, POMDPPolicies, BeliefUpdaters, and/or POMDPSimulators instead.")
+end
+
+end # module
